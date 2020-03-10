9c54a8ebb15726909a130fef087733387775332a
Avery Quinn <avery.quinn1493@gmail.com>
2020-03-09 22:36:18 -0400

    Local Build

---
+++ setup.py
@@ -0,0 +1,21 @@
+
+# setup.py
+
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="avery1493-Lambdata-12",
+    version="1.0",
+    author="Avery Quinn",
+    author_email="avery.quinn1493@gmail.com",
+    description="Example Module",
+    long_description=long_description,
+    long_description_content_type="text/markdown", # required if using a md file for long desc
+    #license="MIT",
+    url="https://github.com/Avery1493/Lambdata-12",
+    #keywords="",
+    packages=find_packages() # ["my_lambdata"]
+)
