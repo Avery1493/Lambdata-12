4e00cad8fa1e197bf15f84554b052e9372cd8047
Avery Quinn <avery.quinn1493@gmail.com>
2020-03-10 10:30:13 -0400

    Add splitdate and temp_conv functions to package

--- my_lambdata/my_mod.py
+++ my_lambdata/my_mod.py
@@ -1,25 +1,74 @@
 
 # my_lambdata/my_mod.py
+import pandas
+from pandas import to_datetime
 
-
-def enlarge(n):
+def splitdate(df, column):
     """
-    Parm n is a number
-    Function will enlarge the number
+    Pram df is a dataframe
+    Pram column is a column header surrounded in quotes
+    the columns should contain date values
+    Function will split the date into multiple columns
     """
-    return n * 100
+    df= df.copy()
+    df[column] = to_datetime(df[column])
+    df['month'] = df[column].dt.month
+    df['day'] = df[column].dt.day
+    df['year'] = df[column].dt.year
+
+    return df
+if __name__ == "__main__":
+    df = pandas.DataFrame({"date": ["6/18/2018","10/5/2020"]})
+    print("output", splitdate(df, "date"))
 
 
-# this code breaks our ability to import enlarge from other files
-# print("HELLO")
-# y = int(input("Please choose a number"))
-# print(y, enlarge(y))
-
+def temp_conv(degrees,metric=True):
+    """
+    Parm degrees is the temperature as integer or float
+    Parm metric is the unit degrees is measured in
+    Default metric is True, meaning Celcius 
+    Metric = False, mean Fahrenheit 
+    """
+    d = degrees
+    if metric == True:
+        temp = (d * 9/5) + 32
+        return temp
+    else:
+        temp = (d - 32) * 5/9
+        return temp
 
 if __name__ == "__main__":
-    # only run this code IF this script is invoked from the command line
-    # not if it is imported from another
-    print("HELLO")
-    y = int(input("Please choose a number"))
-    print(y, enlarge(y))
+    print("output", temp_conv(32,False))
+
+
+
+
+
+
+
+
+
+
+
+#TEST
+# def enlarge(n):
+#     """
+#     Parm n is a number
+#     Function will enlarge the number
+#     """
+#     return n * 100
+
+
+# # this code breaks our ability to import enlarge from other files
+# # print("HELLO")
+# # y = int(input("Please choose a number"))
+# # print(y, enlarge(y))
+
+
+# if __name__ == "__main__":
+#     # only run this code IF this script is invoked from the command line
+#     # not if it is imported from another
+#     print("HELLO")
+#     y = int(input("Please choose a number"))
+#     print(y, enlarge(y))
 
--- my_lambdata/my_script.py
+++ my_lambdata/my_script.py
@@ -3,16 +3,21 @@
 
 import pandas
 
-from my_lambdata.my_mod import enlarge
+from my_lambdata.my_mod import splitdate
+from my_lambdata.my_mod import temp_conv
 
-print("HELLO WORLD")
+df = pandas.DataFrame({"date": ["6/18/2018","10/5/2020"]})
+print("output", splitdate(df, "date"))
 
-df = pandas.DataFrame({"state": ["CT","CO","CA","TX"]})
-print(df.head())
-
-print("--------------")
+print("output", temp_conv(32,False))
 
 
-n = 5
-print("NUMBER",n)
-print("ENLARGED NUMBER", enlarge(n)) #invoking the function
+#python -m my_lambdata.my_script
+#TEST
+# print("HELLO WORLD")
+# df = pandas.DataFrame({"state": ["CT","CO","CA","TX"]})
+# print(df.head())
+# print("--------------")
+# n = 5
+# print("NUMBER",n)
+# print("ENLARGED NUMBER", enlarge(n)) #invoking the function
