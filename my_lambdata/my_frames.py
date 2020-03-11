4a8b1c95940e5a32fb6202973242e1f9ae26b76a
Avery Quinn <avery.quinn1493@gmail.com>
2020-03-11 13:39:32 -0400

    Class practice

--- my_lambdata/my_frames.py
+++ my_lambdata/my_frames.py
@@ -1,9 +1,87 @@
 
 # my_lambdata/my_frames.py
-
-# Sate abbreviation 
-
 import pandas
 
-df = pandas.DataFrame({"state": ["CA", "WA"]})
-print(df.head())
+class DataProcessor():
+    def __init__(self, my_df):
+        self.df = my_df
+
+
+
+def add_state_names(self):
+    names_map = {
+        'AK': 'Alaska',
+        'AL': 'Alabama',
+        'AR': 'Arkansas',
+        'AS': 'American Samoa',
+        'AZ': 'Arizona',
+        'CA': 'California',
+        'CO': 'Colorado',
+        'CT': 'Connecticut',
+        'DC': 'District of Columbia',
+        'DE': 'Delaware',
+        'FL': 'Florida',
+        'GA': 'Georgia',
+        'GU': 'Guam',
+        'HI': 'Hawaii',
+        'IA': 'Iowa',
+        'ID': 'Idaho',
+        'IL': 'Illinois',
+        'IN': 'Indiana',
+        'KS': 'Kansas',
+        'KY': 'Kentucky',
+        'LA': 'Louisiana',
+        'MA': 'Massachusetts',
+        'MD': 'Maryland',
+        'ME': 'Maine',
+        'MI': 'Michigan',
+        'MN': 'Minnesota',
+        'MO': 'Missouri',
+        'MP': 'Northern Mariana Islands',
+        'MS': 'Mississippi',
+        'MT': 'Montana',
+        'NA': 'National',
+        'NC': 'North Carolina',
+        'ND': 'North Dakota',
+        'NE': 'Nebraska',
+        'NH': 'New Hampshire',
+        'NJ': 'New Jersey',
+        'NM': 'New Mexico',
+        'NV': 'Nevada',
+        'NY': 'New York',
+        'OH': 'Ohio',
+        'OK': 'Oklahoma',
+        'OR': 'Oregon',
+        'PA': 'Pennsylvania',
+        'PR': 'Puerto Rico',
+        'RI': 'Rhode Island',
+        'SC': 'South Carolina',
+        'SD': 'South Dakota',
+        'TN': 'Tennessee',
+        'TX': 'Texas',
+        'UT': 'Utah',
+        'VA': 'Virginia',
+        'VI': 'Virgin Islands',
+        'VT': 'Vermont',
+        'WA': 'Washington',
+        'WI': 'Wisconsin',
+        'WV': 'West Virginia',
+        'WY': 'Wyoming'
+    }
+    #breakpoint() # python 3.7 or late, otherwise ud pdb module
+    self.df["name"] = self.df['abbrev'].map(names_map)
+
+
+
+df = pandas.DataFrame({"abbrev": ["CA", "FL", "TX"]})
+df2 = pandas.DataFrame({"abbrev": ["CA", "WA", "CT"]})
+
+for df in [df,df2]:
+    my_frame = DataProcessor(df)
+    print(my_frame.df.head())
+
+
+# mutated_df = add_state_names(df)
+# mutated_df2 = add_state_names(df2)
+# print(mutated_df.head())
+# print(mutated_df2.head())
